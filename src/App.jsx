48f8b8743a82ebf04f0519aa31e92983d9b548cc
Ajamu Page <kil91402@gmail.com>
2023-08-17 22:24:28 -0500

    added some frontend code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-import React from "react"
+import React, { Component } from "react";
+import axios from "axios"; 
 
+class BestBooks extends Component {
+  state = {
+    books: [],
+  };
 
+  async componentDidMount() {
+    try {
+      const response = await axios.get("/books");   
+            this.setState({ books: response.data });
+    } catch (error) {
+      console.error("Error fetching books:", error);
+    }
+  }
 
-export default App;
+  renderBooks() {
+    if (this.state.books.length === 0) {
+      return <p>Out of Books.</p>;
+    }
+
+};
+}
+export default BestBooks;
+

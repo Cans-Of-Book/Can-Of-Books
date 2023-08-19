edf637abf815326fcae36e59dc92ae9c0887ea73
Ajamu Page <kil91402@gmail.com>
2023-08-19 15:29:53 -0500

    added carousel

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Books from "./Books";
 import { Route, Routes } from "react-router-dom";
 import About from "./About";
+import { Carousel } from "bootstrap";
 const backendUrl = import.meta.env.BACKEND_URL || "http://localhost:3001";
 
 class BestBooks extends Component {
@@ -27,11 +28,25 @@ class BestBooks extends Component {
         <div>
           <div>
             {this.state.books.map((book, index) => (
-              <div key={index}>
+              <Carousel>
+              <Carousel.Item key={index}>
+                <img className="d-block w-100"
+                src="https://www.google.com/books/edition/The_Color_Purple/1W8-c_m-noEC?hl=en&gbpv=1&dq=the%20color%20purple&pg=PP1&printsec=frontcover"
+                alt="Slide 1"/>
+                <img className="d-block w-100"
+                src="https://www.google.com/books/edition/The_Silence_of_the_Lambs/zm9vEAAAQBAJ?hl=en&gbpv=1&dq=silence%20of%20the%20lambs&pg=PP1&printsec=frontcover"
+                alt="Slide 2"/>
+                <img className="d-block w-100"
+                src="https://www.google.com/books/edition/Maybe_You_Never_Cry_Again/5ANCSjFRhf8C?hl=en&gbpv=1&pg=PP1&printsec=frontcover"
+                alt="Slide 3"/>
+                <Carousel.Caption>
                   <h3>{book.title}</h3>
                   <p>{book.author}</p>
                   <p>{book.description}</p>
-                </div>
+                  </Carousel.Caption>
+                  </Carousel.Item>
+                  </Carousel>
+                
               ))}
           </div>
         </div>

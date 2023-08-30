95f22eb029fb68f10803c9870f2e11762f22bf10
Ajamu Akil Page <kil91402@gmail.com>
2023-08-30 18:41:27 +0000

    cleaned up code

--- src/Books.jsx
+++ src/Books.jsx
@@ -1,38 +1,17 @@
-// import React, { Component } from 'react'
-
-// export default class Books extends Component {
-//     render() {
-//       let bookComponents = this.props.books.map((book) => (
-//         <div key={books_id}>
-//           {book.Title}
-//         </div>
-//       ))
-//         if (this.props.books.length === 0)
-//          {
-//           return <p>Out of Books.</p>;
-//         }
-//         return(
-//           <div>Found Books</div>
-//           {bookComponents}
-//         )
-//     };
-// }
-
-
 import React, { Component } from 'react';
 
 export default class Books extends Component {
   render() {
     let bookComponents = this.props.books.map((book) => (
-      <div key={book.id}>  {/* Assuming book object has an 'id' property */}
-        {book.Title}  {/* Make sure the data structure actually uses 'Title' with an uppercase 'T' */}
+      <div key={book.id}>
+        {book.Title}
       </div>
     ));
-    
+
     if (this.props.books.length === 0) {
       return <p>Out of Books.</p>;
     }
-    
+
     return (
       <div>
         <div>Found Books</div>
